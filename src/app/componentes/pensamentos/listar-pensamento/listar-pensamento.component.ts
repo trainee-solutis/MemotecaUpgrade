d2ccec8e7c87f6ed777a39210b4a0e0d25529f4e
4ntonio19 <vgoncalvescarvalho@outlook.com>
2023-10-05 21:39:58 -0300

    Template Deny

---
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -0,0 +1,42 @@
+import { PensamentoService } from './../pensamento.service';
+import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
+
+@Component({
+  selector: 'app-listar-pensamento',
+  templateUrl: './listar-pensamento.component.html',
+  styleUrls: ['./listar-pensamento.component.css']
+})
+export class ListarPensamentoComponent implements OnInit {
+
+  listaPensamentos: Pensamento[] = [];
+
+  haMaisPensamentos: boolean = true;
+  paginaAtual: number = 1;
+  filtro: string = ''
+
+  constructor(private service: PensamentoService) { }
+
+  ngOnInit(): void {
+    this.service.listar(this.paginaAtual, this.filtro).subscribe((listaPensamentos) => {
+      this.listaPensamentos = listaPensamentos
+    })
+  }
+
+  carregarMaisPensamentos(){
+    this.service.listar(++this.paginaAtual, this.filtro)
+    .subscribe(listaPensamentos =>{
+      this.listaPensamentos.push(...listaPensamentos);
+      if(!listaPensamentos.length){
+        this.haMaisPensamentos = false;
+      }
+    })
+  }
+
+  pesquisarPensamentos(){
+    this.haMaisPensamentos = true
+    this.paginaAtual = 1;
+    this.service.listar(this.paginaAtual, this.filtro).subscribe(listaPensamentos => {this.listaPensamentos = listaPensamentos})
+  }
+}
+
