238a83c56099d596a4dc1af6c5fae113e7cd2615
MaateusMDS <maateusmania@gmail.com>
2023-10-13 16:20:34 -0300

    perf: made a script to run and build

--- backend/email-sender/index.js
+++ backend/email-sender/index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const nodemailer = require('nodemailer');
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.use(bodyParser.json());
 app.use(cors());
